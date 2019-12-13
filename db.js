be7d0c768ca2a94db38052ad09cd8a9101363829
Hamza Shahid <hammzzeeee@gmail.com>
2019-12-13 08:19:11 +0000

    stored db connection string and port in .env

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 const dotenv = require("dotenv").config()
 const mongodb = require("mongodb")
 
-const connectionString = ""
-
-mongodb.connect(connectionString, 
+mongodb.connect(process.env.CONNECTION_STRING, 
   {useNewUrlParser: true, useUnifiedTopology: true}, 
   function(err, client) {
     module.exports = client.db()
     const app = require("./app")
-    app.listen(3000, function() {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running on port 3000")
     })
 })
