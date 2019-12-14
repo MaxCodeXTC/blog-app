f73e2b887568f43dba57a369b79f5338c9707757
Hamza Shahid <hammzzeeee@gmail.com>
2019-12-14 20:06:30 +0000

    user login feature semi complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,8 @@ exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
     res.send(result)
-  }).catch(function(error) {
-    res.send(error)
+  }).catch(function(err) {
+    res.send(err)
   })
 }
 
