7f1d0702b570cc1eb4e05d93860a330ca3c8fe21
Hamza Shahid <hammzzeeee@gmail.com>
2019-12-23 21:55:55 +0000

    implemented logout feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,14 +8,18 @@ exports.login = function(req, res) {
       loginInfo: "initial",
       username: user.data.username //coming from User.js in the login prototype method.
     }
-    res.send(result)
+    req.session.save(function() {
+      res.redirect("/")
+    })
   }).catch(function(e) {
     res.send(e)
   })
 }
 
-exports.logout = function() {
-  
+exports.logout = function(req, res) {
+  req.session.destroy(function() {
+    res.redirect("/")
+  })
 }
 
 exports.register = function(req, res) {
